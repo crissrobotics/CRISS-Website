1c2e73f0dd195b70a53d77ba7737ccc8d2def078
Manas <manasck14@gmail.com>
2024-07-11 03:41:53 +0530

    "feat: added about section"

---
+++ src/components/Home/About/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./about.scss";
+
+import aboutPic from "../../../assets/images/Homepage/about.jpeg"
+
+export default function About() {
+
+    return (
+        <div className="aboutContainer">
+            <h2>About<span>Us</span></h2>
+            <div className="details">
+                <div className="imgContainer"><img src={aboutPic}></img></div>
+                <p>Criss Robotics, a dynamic and innovative Mars rover-making club at <span>BITS Pilani</span>  formed in 2021. 
+                    We are multidisciplinary space engineering and Robotics Research group in BITS Pilani 
+                    (Pilani campus) consisting of 60+ tech enthusiasts with a vision to innovate and develop space 
+                    exploration projects.
+                </p>
+            </div>
+        </div>
+
+    )
+}
