0d35970ecdc574225e429dd4c2b0315132af0766
Manas <manasck14@gmail.com>
2024-07-11 03:40:59 +0530

    "feat: added mission section"

---
+++ src/components/Home/Mission/Mission.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./mission.scss";
+
+import missionImg from "../../../assets/images/Homepage/mission1.jpeg";
+export default function Mission() {
+
+    return (
+        <div className="missionContainer">
+
+            <div className="details">
+                <div className="left">
+                    <h2>Our<span>Mission</span></h2>
+                    <p>We as a team are interested in research about space and robotics. Our current goal is to design
+                        and develop rovers to compete in global student competitions such as IRC IRDC URC
+                    </p>
+                </div>
+                <div className="imgContainer"><img src={missionImg}></img></div>
+            </div>
+        </div>
+    )
+}
