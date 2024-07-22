93754a7c931935c0eb75b427e5542a0475778205
Manas <manasck14@gmail.com>
2024-07-23 00:22:29 +0530

    added alumni members

--- src/components/Alumni/alumniData.js
+++ src/components/Alumni/alumniData.js
@@ -1,13 +1,172 @@
+import a1 from "../../assets/images/Alumni/2021/a1.jpeg";
+import a2 from "../../assets/images/Alumni/2021/a2.jpg";
+import a3 from "../../assets/images/Alumni/2021/a3.png";
+import a4 from "../../assets/images/Alumni/2021/a4.jpg";
+import a5 from "../../assets/images/Alumni/2021/a5.jpg";
+import a6 from "../../assets/images/Alumni/2021/a6.jpg";
+import a7 from "../../assets/images/Alumni/2021/a7.jpg";
+import a8 from "../../assets/images/Alumni/2021/a8.png";
+import a9 from "../../assets/images/Alumni/2021/a9.jpg";
+import a11 from "../../assets/images/Alumni/2021/a11.jpg";
+import a12 from "../../assets/images/Alumni/2021/a12.jpeg";
+import a13 from "../../assets/images/Alumni/2021/a13.jpeg";
+import a14 from "../../assets/images/Alumni/2021/a14.jpg";
+import a15 from "../../assets/images/Alumni/2021/a15.jpg";
+import a16 from "../../assets/images/Alumni/2021/a16.jpg";
+
 export const Alumnis = [
 
     {
         id: "1",
-        name: "",
-        img: "",
+        name: "Dev Parikh",
+        img: a1,
         position: "Team Captain",
-        linkedin: "",
+        linkedin: "https://www.linkedin.com/in/dev-parikh9/",
         team: "",
-        batch: ""
+        batch: "2021"
     },
-
+    {
+        id: "2",
+        name: "Mahip Gorana",
+        img: a2,
+        position: "Management Lead",
+        linkedin: "https://www.linkedin.com/in/mahip-gorana-02b150252",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "3",
+        name: "Shuswabhit Shadangi",
+        img: a3,
+        position: "Chief Technical Officer",
+        linkedin: "https://www.linkedin.com/in/shuswabhit-shadangi/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "4",
+        name: "Shubham Gupta",
+        img: a4,
+        position: "Science Lead",
+        linkedin: "https://www.linkedin.com/in/shubham-gupta-389491230/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "5",
+        name: "Shobhit Rathi",
+        img: a5,
+        position: "Operations Lead",
+        linkedin: "https://www.linkedin.com/in/shobhit-rathi-826216240/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "6",
+        name: "Vedant Kabra",
+        img: a6,
+        position: "Electrical Team Lead",
+        linkedin: "https://www.linkedin.com/in/vedant-kabra-7b74821b0/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "7",
+        name: "Mathai Mathew Pulicken",
+        img: a7,
+        position: "Software Lead",
+        linkedin: "https://in.linkedin.com/in/mathai-mathew-pulicken",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "8",
+        name: "Aryaman Agrawal",
+        img: a8,
+        position: "Astrobio Lead",
+        linkedin: "https://www.linkedin.com/in/aryaman-agrawal-6100a7261/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "9",
+        name: "Rishi Agarwal",
+        img: a9,
+        position: "Software Lead",
+        linkedin: "https://www.linkedin.com/in/rishiagarwal01/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "10",
+        name: "Aadith Warrier",
+        img: a8,
+        position: "Mechanical Lead",
+        linkedin: null,
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "11",
+        name: "Aman Gupta",
+        img: a11,
+        position: "Mobility Lead",
+        linkedin: "https://www.linkedin.com/in/aman-gupta-996907223/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "12",
+        name: "Devchand Bothra",
+        img: a12,
+        position: "Arm Lead",
+        linkedin: "https://www.linkedin.com/in/devchand-bothra-60b5ab226/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "13",
+        name: "Ashmit Srivastava",
+        img: a13,
+        position: "Web Dev Lead",
+        linkedin: "https://www.linkedin.com/in/ashmit-srivastava/",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "14",
+        name: "Akshita Raj Pandita",
+        img: a14,
+        position: "Social Media and Content Lead",
+        linkedin: "https://www.linkedin.com/in/akshita-raj-pandita-4a9571210",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "15",
+        name: "Ojasva Goyal",
+        img: a15,
+        position: "Head of Events",
+        linkedin: "https://www.linkedin.com/in/ojasva-goyal-476422230",
+        team: "",
+        batch: "2021"
+    },
+    {
+        id: "16",
+        name: "Abhinav Mathur",
+        img: a16,
+        position: "DAV lead",
+        linkedin: "https://www.linkedin.com/in/abhinav-mathur-bab966233/",
+        team: "",
+        batch: "2021"
+    },
+    // {
+    //     id: "16",
+    //     name: "",
+    //     img: "",
+    //     position: "",
+    //     linkedin: "",
+    //     team: "",
+    //     batch: "2021"
+    // }
 ]
