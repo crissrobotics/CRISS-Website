9952ba8c1a4a1bda24572c6e27a8667302f5c2cc
Manas <manasck14@gmail.com>
2024-07-22 18:47:23 +0530

    changed map location

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -69,7 +69,7 @@ export default function Contact() {
                         <a target="_blank" rel="noopener noreferrer" href="tel:+917000868748">+91 7000868748</a>
                     </div>
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3510.9908613146695!2d75.58404392695313!3d28.359125899999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39131964f43e4575%3A0x1fbad30854cf884d!2sBirla%20Institute%20of%20Technology%20And%20Science%20-%20Pilani!5e0!3m2!1sen!2sin!4v1720702371484!5m2!1sen!2sin"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3510.837304999576!2d75.58706707373962!3d28.363767996237954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391319d22fc7bb49%3A0x6d199729bd3a1bb1!2sCRISS%20Robotics!5e0!3m2!1sen!2sin!4v1721654098436!5m2!1sen!2sin"
                         width="542"
                         height="346"
                         style={{ border: 0 }}
