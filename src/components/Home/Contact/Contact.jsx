ecfe1adeadb38bab7ce0ae35bb794c1e4ebc06ed
Manas <manasck14@gmail.com>
2024-07-23 22:21:52 +0530

    feat: responsive ness of contact us

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -70,7 +70,6 @@ export default function Contact() {
                     </div>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3510.837304999576!2d75.58706707373962!3d28.363767996237954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391319d22fc7bb49%3A0x6d199729bd3a1bb1!2sCRISS%20Robotics!5e0!3m2!1sen!2sin!4v1721654098436!5m2!1sen!2sin"
-                        width="542"
                         height="346"
                         style={{ border: 0 }}
                         allowFullScreen=""
