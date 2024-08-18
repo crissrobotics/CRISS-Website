a7a7ecb0c91e1241fe4c1bda95de544077fd2f74
Manas <manasck14@gmail.com>
2024-08-18 22:57:43 +0530

    added devpage members

--- src/components/Developer/DevData.js
+++ src/components/Developer/DevData.js
@@ -1,4 +1,7 @@
 import manaschoudhary from "../../assets/images/Team/2023/Management/manaschoudhary.webp";
+import sannidhkumar from "../../assets/images/Team/2023/Management/sannidhkumar.webp";
+import aishwarya from "../../assets/images/Team/2023/Management/aishwarya.webp";
+import yashwanthvarma from "../../assets/images/Team/2023/Management/yashwanthvarma.webp";
 
 export const Developers = [
     
@@ -11,4 +14,41 @@ export const Developers = [
         github: "https://github.com/ManasChoudhary-01",
         insta: "https://www.instagram.com/manaschoudhary_?igsh=YTQwZjQ0NmI0OA==",
     },
+    {
+        id: "2",
+        name: "Agrim Gupta",
+        img: "",
+        dev: "Frontend Developer",
+        linkedin: "",
+        github: "",
+        insta: "",
+    },
+    {
+        id: "3",
+        name: "Aishwarya",
+        img: aishwarya,
+        dev: "UI/UX Designer",
+        linkedin: "https://www.linkedin.com/in/aishwarya-singh-k",
+        github: null,
+        insta: "https://www.instagram.com/instanssb?igsh=MW5vcDQ5bTA5eTBtNw==",
+    },
+    {
+        id: "4",
+        name: "Saniddh Kumar",
+        img: sannidhkumar,
+        dev: "UI/UX Designer",
+        linkedin: null,
+        github: null,
+        insta: "https://www.instagram.com/_saniddh_?igsh=Yjd3djF2dXdmdXNi&utm_source=qr",
+    },
+    {
+        id: "5",
+        name: "Yashwanth Varma",
+        img: yashwanthvarma,
+        dev: "UI/UX Designer",
+        linkedin: "https://www.linkedin.com/in/yashwanth-varma-dandu-2b3608292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: null,
+        insta: "https://www.instagram.com/yashwanth2118?igsh=eHB5cTNzaTdtdTVh",
+    },
+
 ]
