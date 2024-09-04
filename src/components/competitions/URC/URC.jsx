50c3e3c91c026dc9d5b62410076e9c6d003b676f
TheClusterBond <f20230232@pilani.bits-pilani.ac.in>
2024-09-05 00:16:42 +0530

    trying9

--- src/components/competitions/URC/URC.jsx
+++ src/components/competitions/URC/URC.jsx
@@ -6,7 +6,6 @@ import backImg from "../../../assets/images/Homepage/background.jpg";
 import backImg1 from "../../../assets/images/Homepage/backgroundlarge.jpeg";
 import { motion } from "framer-motion";
 import { fadeAnimation, slideAnimation } from "../../Header/Motion/Motion";
-import URCVIDEO from "../../../assets/images/Competitions/URCVIDEO.mp4";
 
 export default function urc() {
 
@@ -40,9 +39,22 @@ export default function urc() {
                 <motion.p {...slideAnimation("right", 0.5)} className={styles.urcText}><span>The University Rover Challenge (URC)</span> is the world's premier robotics competition for college students.  Held annually in the desert of southern Utah in the United States, URC challenges student teams to design and build the next generation of <span>Mars rovers</span> that will one day work alongside astronauts exploring the Red Planet.</motion.p>
             </div>
 
-            <motion.div className={styles.videoMain} {...slideAnimation("left", 0.3)}>
+            <div className={styles.videoMain} {...slideAnimation("left", 0.3)}>
+                <iframe
+                    className={styles.vid}
+
+                    src="https://www.youtube.com/embed/gIik18I17i8?si=bntKgGhQpQnOOaTQ" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                </iframe>
+            </div>
+
+            {/* <motion.div className={styles.videoMain} {...slideAnimation("left", 0.3)}>
                 <video className={styles.vid} src={URCVIDEO} autoPlay loop ></video>
-            </motion.div>
+            </motion.div> */}
 
             <motion.div className={styles.The2023Container} style={{ backgroundImage: `url(${backgroundImage})` }}    {...slideAnimation("up", 0.5)}>
                 <URC2023></URC2023>
