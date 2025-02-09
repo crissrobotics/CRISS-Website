4700b31631eba9fcd2725653b512d0ef44100997
TheClusterBond <f20230232@pilani.bits-pilani.ac.in>
2025-02-09 13:17:26 +0530

    making changes in dev tab

--- src/components/Developer/DevData.js
+++ src/components/Developer/DevData.js
@@ -1,9 +1,7 @@
 import manaschoudhary from "../../assets/images/Team/2023/Management/manaschoudhary.webp";
 import sannidhkumar from "../../assets/images/Team/2023/Management/sannidhkumar.webp";
 import aishwarya from "../../assets/images/Team/2023/Management/aishwarya.webp";
-import yashwanthvarma from "../../assets/images/Team/2023/Management/yashwanthvarma.webp";
 import agrimgupta from "../../assets/images/Team/2023/Management/agrimgupta.webp";
-import aditiagarwal from "../../assets/images/Team/2023/Management/aditiagarwal.webp";
 
 export const Developers = [
     
@@ -42,24 +40,5 @@ export const Developers = [
         linkedin: "https://www.linkedin.com/in/saniddh-kumar-729069281?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
         github: null,
         insta: "https://www.instagram.com/_saniddh_?igsh=Yjd3djF2dXdmdXNi&utm_source=qr",
-    },
-    {
-        id: "5",
-        name: "Yashwanth Varma",
-        img: yashwanthvarma,
-        dev: "UI/UX Designer",
-        linkedin: "https://www.linkedin.com/in/yashwanth-varma-dandu-2b3608292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: null,
-        insta: "https://www.instagram.com/yashwanth2118?igsh=eHB5cTNzaTdtdTVh",
-    },
-    {
-        id: "23",
-        name: "Aditi Agarwal",
-        img: aditiagarwal,
-        dev: "UI/UX Designer",
-        linkedin: "https://www.linkedin.com/in/aditi-agarwal-6012142a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: null,
-        insta: "https://www.instagram.com/aditiag_07?igsh=aHIyNTQ4aGFqejE0",
-    },
-
+    }
 ]
